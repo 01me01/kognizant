c4fc487a6372fcecc9c132442aa682d20472f288
01me01 <96710377+01me01@users.noreply.github.com>
2023-06-10 00:05:33 +0530

    Update firebase_config.js

--- src/firebase_config.js
+++ src/firebase_config.js
@@ -2,7 +2,7 @@ import { initializeApp } from "firebase/app";
 import {getAuth} from "firebase/auth";
 
 const firebaseConfig = {
-    apiKey: "AIzaSyBtzWZFEBMRRCuZTBxRnOEalExtKuZWfEI",
+    apiKey: " ",
     authDomain: "kognizant-b4d57.firebaseapp.com",
     projectId: "kognizant-b4d57",
     storageBucket: "kognizant-b4d57.appspot.com",
